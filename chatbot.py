c4ab930634b94378cb911ec6e1d62b4f53a75477
Eduardo Soares Dutra <eduardosoaresdutra@gmail.com>
2022-11-07 00:25:50 -0300

    Adicionadas novas funções e arquivo de armazenamento de telefones

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,7 @@
+def setPhoneNumber():
+    ## Verificar telefones na lista de clientes
+    pass
+
 def firstMessage():
     print('Olá! Seja bem-vindo a WE-RJ Telecom!')
 
@@ -9,4 +13,24 @@ def firstMessage():
 
     return newUser
 
-firstMessage()
+def registeredClient():
+    print('Olá Fulano, bem-vindo a WE-RJ Telecom!')
+
+    userInputString = str(input('O que você precisa?\nCaso queira contratar ou trocar de plano escreva “Quero contratar” ou “Quero trocar de plano”:\n'))
+
+    userInputString = userInputString.lower()
+
+    if userInputString == True:
+        if 'contratar' in userInputString:
+            print('Ótimo! Temos os seguintes planos:\n200 Mbp/s\n400 Mbp/s\n500 Mbp/s\n1 Gbp/s\n')
+
+            userOptionInput = str(input('Caso algum desses planos te interesse, escreva “Tenho interesse”, que você será redirecionado para um atendente.\nOu caso queira voltar para o menu anterior, digite “Voltar”.')).lower()
+
+            if 'voltar' in userOptionInput:
+                registeredClient()
+            elif '200' in userOptionInput:
+                pass
+            else:
+                print('Não entendi a sua mensagem, poderia digitar novamente?')
+                ## chamar função novamente
+
