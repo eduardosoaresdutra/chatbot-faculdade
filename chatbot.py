67a078145ef7d4232be86a6236e9a4af540f4556
Eduardo Soares Dutra <eduardosoaresdutra@gmail.com>
2022-11-20 00:14:25 -0300

    Terminada função de escolha de plano

--- chatbot.py
+++ chatbot.py
@@ -13,6 +13,22 @@ def firstMessage():
 
     return newUser
 
+def newPlan(userChoice):
+    pass
+
+def newPlanOption():
+    print('Ótimo! Temos os seguintes planos:\n200 Mbp/s\n400 Mbp/s\n500 Mbp/s\n1 Gbp/s\n')
+
+    userInternetSpeedOption = str(input('Caso algum desses planos te interesse, escreva “Tenho interesse”, que você será redirecionado para um atendente.\nOu caso queira voltar para o menu anterior, digite “Voltar”.\n')).lower()
+
+    if 'voltar' in userInternetSpeedOption:
+        registeredClient()
+    elif '200' or '400' or '500' or '1' or '1 gb' or '1 gbps' or '1 gbp/s' or 'interesse' or 'quero' or 'interessado' in userInternetSpeedOption:
+        newPlan(userInternetSpeedOption)
+    else:
+        print('Não entendi a sua mensagem, poderia digitar novamente?')
+        newPlanOption()
+
 def registeredClient():
     print('Olá Fulano, bem-vindo a WE-RJ Telecom!')
 
@@ -20,17 +36,7 @@ def registeredClient():
 
     userInputString = userInputString.lower()
 
-    if userInputString == True:
-        if 'contratar' in userInputString:
-            print('Ótimo! Temos os seguintes planos:\n200 Mbp/s\n400 Mbp/s\n500 Mbp/s\n1 Gbp/s\n')
+    if 'contratar' in userInputString:
+        newPlanOption()
 
-            userOptionInput = str(input('Caso algum desses planos te interesse, escreva “Tenho interesse”, que você será redirecionado para um atendente.\nOu caso queira voltar para o menu anterior, digite “Voltar”.')).lower()
-
-            if 'voltar' in userOptionInput:
-                registeredClient()
-            elif '200' in userOptionInput:
-                pass
-            else:
-                print('Não entendi a sua mensagem, poderia digitar novamente?')
-                ## chamar função novamente
-
+registeredClient()
